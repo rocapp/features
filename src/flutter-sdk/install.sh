acdf345d5f65c05b1f696876955bc72ce9e3dc7a
Robbie Capps <rocapp@gmail.com>
2023-08-01 15:48:44 -0700

    Update install.sh

--- src/flutter-sdk/install.sh
+++ src/flutter-sdk/install.sh
@@ -39,11 +39,11 @@ HASH=$(jq ".current_release.$RELEASE" $RELEASES_JSON)
 FLUTTER_ARCHIVE=$(jq -r ".releases[] | select(.hash==$HASH) | .archive" releases_linux.json)
 curl -O "$RELEASES_URL/$FLUTTER_ARCHIVE" &&
   tar -xf "$TMP_DIR/$(basename "$FLUTTER_ARCHIVE")" -C "$(dirname "$FLUTTER_HOME")" &&
-    chown --recursive "$_REMOTE_USER:$_REMOTE_USER" "$FLUTTER_HOME" &&
+    chown --recursive "$USER:$USER" "$FLUTTER_HOME" &&
       git config --global --add safe.directory "$FLUTTER_HOME"
 
 # Clean up
-cd / && 
+cd "~" && 
   rm -rf "$TMP_DIR" &&
     apt clean
 
